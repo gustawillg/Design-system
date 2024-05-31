7275394532c9b5d9c7f64c81f0cbc53aa2bffd25
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-05-30 21:09:52 -0300

    Configurando pacote do React

--- packages/react/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { colors } from '@marci-ui/tokens'
-
-console.log(colors)
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,6 @@
+import { colors } from '@marci-ui/tokens'
+import React from 'react'
+
+export function App() {
+  return <h1 style={{ color: colors.ignite300 }}>Hello</h1>
+}
