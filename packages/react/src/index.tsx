2c7e7e5dac23d87ad273ab0d7b6c02293146e50e
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-05-30 22:06:31 -0300

    Configuração do Stitches

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,13 @@
-import { colors } from '@marci-ui/tokens'
 import React from 'react'
+import { styled } from './styles'
+
+const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+  padding: '$4',
+})
 
 export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello</h1>
+  return <Button>Hello</Button>
 }
