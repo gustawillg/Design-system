6b5458dee8062ce3b359aa103b7debeb38a29c34
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-06-01 18:39:18 -0300

    dark theme

--- node_modules/docs/.storybook/main.js
+++ node_modules/docs/.storybook/main.js
@@ -1,21 +1,25 @@
 module.exports = {
   "stories": [
     "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
+    "../src/stories/*.stories.tsx"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
+
+  "framework": {
+    name: "@storybook/react-vite",
+    options: {}
   },
+
   "features": {
     "storyStoreV7": true
   },
+
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/Design-system/'
