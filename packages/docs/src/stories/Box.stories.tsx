4a1a961c2c4439805b86dc5345ad38fca6b23093
Gustavo <122057283+gustawillg@users.noreply.github.com>
2024-06-11 17:07:27 -0300

    Addon de acessibilidade

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -2,18 +2,11 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Box, BoxProps, Text } from '@marci-ui/react'
 
 export default {
-    title: 'Surfaces/Box',
-    component: Box,
-    args: {
-        children: <Text>Testando o elemento Box</Text>,
-    },
-    argTypes: {
-        children: {
-            control: {
-                type: null,
-            },
-        },
-    },
-} as unknown as Meta<BoxProps>
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
+} as Meta<BoxProps>
 
 export const Primary: StoryObj<BoxProps> = {}
